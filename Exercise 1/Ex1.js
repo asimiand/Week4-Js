a202357710b87f12e99a5a1a3f885e733b0d21db
Adriana Simiand <asimiand@gmail.com>
2020-10-26 06:47:41 -0300

    Exercise 1

---
+++ Exercise 1/Ex1.js
@@ -0,0 +1,28 @@
+//Variables y Operadores
+
+//Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var num1 = 1;
+var num2 = 2;
+
+var sum = num1 + num2;
+
+console.log(sum);
+
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var name1 = 'Marie';
+var lastName1 = 'Curie';
+
+var completeName = name1 + lastName1;
+
+console.log(completeName);
+
+//Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+
+var name2 = 'Helga';
+var lastName2 = 'Patacky';
+
+var nameLength = name2.length + lastName2.length;
+
+console.log(nameLength);
